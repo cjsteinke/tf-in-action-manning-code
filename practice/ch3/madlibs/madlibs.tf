bb9e6b264dbd415cd64f0fe89f5214bd181f2442
Chris Steinke <chrisjsteinke@gmail.com>
2025-01-18 21:45:39 -0600

    Completed Chapter 3

--- practice/ch3/madlibs/madlibs.tf
+++ practice/ch3/madlibs/madlibs.tf
@@ -25,35 +25,62 @@ variable "words" {
   }
 }
 
+variable "num_files" {
+  default = 100
+  type    = number
+}
 
-  resource "random_shuffle" "random_nouns" {
+locals {
+  uppercase_words = {for k, v in var.words : k => [for s in v : upper(s)]}
+}
+
+locals {
+  templates = tolist(fileset(path.module, "templates/*.txt"))
+}
+
+resource "random_shuffle" "random_nouns" {
+    count = var.num_files
     input = var.words["nouns"]
-  }
+}
 
-  resource "random_shuffle" "random_adjectives" {
+resource "random_shuffle" "random_adjectives" {
+    count = var.num_files
     input = var.words["adjectives"]
-  }
+}
 
-  resource "random_shuffle" "random_verbs" {
+resource "random_shuffle" "random_verbs" {
+    count = var.num_files
     input = var.words["adjectives"]
-  }
+}
 
-  resource "random_shuffle" "random_adverbs" {
+resource "random_shuffle" "random_adverbs" {
+    count = var.num_files
     input = var.words["adverbs"]
-  }
+}
 
-  resource "random_shuffle" "random_numbers" {
+resource "random_shuffle" "random_numbers" {
+    count = var.num_files
     input = var.words["numbers"]
-  }
+}
+
+resource "local_file" "mad_libs" {
+  count    = var.num_files
+  filename = "madlibs/madlibs-${count.index}.txt"
+  content = templatefile(element(local.templates, count.index),
+    {
+      nouns      = random_shuffle.random_nouns[count.index].result
+	    adjectives = random_shuffle.random_adjectives[count.index].result
+	    verbs      = random_shuffle.random_verbs[count.index].result
+	    adverbs    = random_shuffle.random_adverbs[count.index].result
+	    numbers    = random_shuffle.random_numbers[count.index].result
+    })
+}
+
+data "archive_file" "mad_libs" {
+  depends_on  = [local_file.mad_libs]
+  type        = "zip"
+  source_dir  = "${path.module}/madlibs"
+  output_path = "${path.cwd}/madlibs.zip"    
+} 
 
 
-  output "mad_libs" {
-    value = templatefile("${path.module}/templates/alice.txt",
-      {
-	nouns      = random_shuffle.random_nouns.result
-	adjectives = random_shuffle.random_adjectives.result
-	verbs      = random_shuffle.random_verbs.result
-	adverbs    = random_shuffle.random_adverbs.result
-	numbers    = random_shuffle.random_numbers.result
-      })
-  }
